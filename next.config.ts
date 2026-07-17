b68f7687120e9c7d1bf278a720ae2ae211a3e02f
Harshvardhan1803 <harshvardan@appicsoftwares.in>
2026-07-17 12:58:26 +0530

    feat: configure remote image patterns in next.config.ts for local and external assets

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "5000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
